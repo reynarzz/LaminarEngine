09bb912cac4b7583485a510562d3c0361c13e582
Reynardo Perez <reynardohiraldo@hotmail.com>
2026-01-15 17:56:23 -0400

    -adds spirv cross library. -compiling slang to glsl es 300 wip

--- GameCooker/Processors/GenericProcessors/SlangShaderAssetProcessor.cs
+++ GameCooker/Processors/GenericProcessors/SlangShaderAssetProcessor.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using SPIRVCross.NET;
 
 namespace GameCooker
 {
@@ -20,30 +21,66 @@ namespace GameCooker
 
             Console.WriteLine("Compile slang: " + path);
 
-            var args = new string[]
+            var target = "spirv";
+            var vertexEntryPoint = "vertexMain";
+            var fragmentEntryPoint = "fragmentMain";
+
+            var vertexArgs = new string[]
             {
                 path,
-                "-profile", "sm_6_6",
-                "-matrix-layout-column-major",
-                "-entry","vertexMain", "-stage", "vertex",
-                "-entry","fragmentMain", "-stage", "pixel",
-                "-target", "spirv"
+                "-profile", "glsl_330", // "sm_6_6",
+                //"-matrix-layout-column-major",
+                "-entry",$"{vertexEntryPoint}", "-stage", "vertex",
+                //"-entry","fragmentMain", "-stage", "pixel",
+                "-target", $"{target}",
+                "-capability", "glsl_spirv_1_6",
+                "-capability", "vertex"
             };
 
-           // var arg = $"slangc {path} -entry vertexMain -stage vertex -entry fragmentMain -stage fragment -target spirv";
+            var fragmentArgs = new string[]
+            {
+                path,
+                "-profile", "glsl_330",
+                //"-matrix-layout-column-major",
+                //"-entry","vertexMain", "-stage", "vertex",
+                "-entry",$"{fragmentEntryPoint}", "-stage", "fragment",
+                "-target", $"{target}",
+                "-capability", "glsl_spirv_1_6",
+                "-capability", "fragment"
+            };
 
+            // var arg = $"slangc {path} -entry vertexMain -stage vertex -entry fragmentMain -stage fragment -target spirv";
 
-            byte[] spv = SlangCompiler.CompileWithReflection(args, out SlangReflection reflection);
+            var vertSpirv = SlangCompiler.CompileWithReflection(vertexArgs, out SlangReflection reflection);
+            var fragSpirv = SlangCompiler.CompileWithReflection(fragmentArgs, out reflection);
 
-
-           // Console.WriteLine($"Compilation Time: {stopwatch.ElapsedMilliseconds} ms");
-            Console.WriteLine($"SPIR-V: {spv.Length} bytes");
+            // Console.WriteLine($"Compilation Time: {stopwatch.ElapsedMilliseconds} ms");
+            Console.WriteLine($"SPIR-V: {vertSpirv.Length} bytes");
             Console.WriteLine($"Reflection JSON: {reflection.Json}");
 
             reflection.Deserialize();
+            var context = new SPIRVCross.NET.Context();
 
-            //SPIRVCross.NET.
-           
+            string CompileGLSL(byte[] spirv)
+            {
+                var parsedIR = context.ParseSpirv(spirv);
+
+                var compiler = context.CreateGLSLCompiler(parsedIR);
+                compiler.glslOptions.ES = true;
+                compiler.glslOptions.version = 300;
+                compiler.glslOptions.separateShaderObjects = false;
+                compiler.glslOptions.vulkanSemantics = false;
+                compiler.glslOptions.enableRowMajorLoadWorkaround = false;
+                compiler.glslOptions.enable420PackExtension = false;
+
+                return compiler.Compile();
+
+            }
+            var vertex = CompileGLSL(vertSpirv);
+            var fragment = CompileGLSL(fragSpirv);
+
+            File.WriteAllText(path + "v_.glsl", vertex);
+            File.WriteAllText(path + "f_.glsl", fragment);
 
             return Encoding.UTF8.GetBytes(_sb.ToString().TrimStart());
         }
