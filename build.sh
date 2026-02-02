75129d38756c40cea10faabb08021aa734fb5825
Reynardo Perez <reynardohiraldo@hotmail.com>
2026-02-01 22:18:41 -0400

    adjusts install path

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ cd platforms/Ios
 dotnet build -p:BuildIOS=true
 
 #install to device:
-xcrun devicectl device install app --device 0000 Entry_Ios.app
+xcrun devicectl device install app --device 0000 bin/Debug/net8.0-ios/ios-arm64/Entry_IOS.app
 
