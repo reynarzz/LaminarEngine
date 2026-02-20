f678549cbc34a22d9981bebb6e1b5642fa2d1c74
Reynardo Perez <reynardohiraldo@hotmail.com>
2026-02-20 11:41:36 -0400

    adds runtime core lib for release ship builds

---
+++ Engine/Third/Native/runtimecore/RuntimeCoreAPI.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#if defined(_WIN32) || defined(_WIN64)
+	#ifdef RUNTIMECORE_EXPORTS
+		#define RUNTIMECORE_API extern "C" __declspec(dllexport)
+	#else
+		#define RUNTIMECORE_API extern "C" __declspec(dllimport)
+	#endif
+#elif defined(__APPLE__) || defined(__linux__)
+	#ifdef RUNTIMECORE_EXPORTS
+		#define RUNTIMECORE_API extern "C" __attribute__((visibility("default")))
+	#else
+		#define RUNTIMECORE_API extern "C"
+	#endif
+#else
+	#define RUNTIMECORE_API extern "C"
+#endif
