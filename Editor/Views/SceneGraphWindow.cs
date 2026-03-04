4d6b70c5555ae071e134d95918f5624990e52c75
Reynardo Perez <reynardohiraldo@hotmail.com>
2026-03-04 14:17:23 -0400

    changes selected actor color

--- Editor/Views/SceneGraphWindow.cs
+++ Editor/Views/SceneGraphWindow.cs
@@ -15,118 +15,18 @@ namespace Editor.Views
         public SceneGraphWindow() : base("Window/Scene Graph")
         {
         }
-
         public override void OnDraw()
         {
+
             if (OnBeginWindow("Scene graph"))
             {
                 ImGui.PushStyleVar(ImGuiStyleVar.ItemSpacing, new Vector2());
-
-                void DrawActor(Actor actor)
-                {
-                    ImGui.PushID(actor.GetID().ToString());
-
-                    bool hasChildren = actor.Transform.Children.Count > 0;
-
-                    ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags.OpenOnArrow | ImGuiTreeNodeFlags.SpanFullWidth;
-
-                    if (!hasChildren)
-                        flags |= ImGuiTreeNodeFlags.Leaf | ImGuiTreeNodeFlags.NoTreePushOnOpen;
-
-                    if (Selector.Selected && Selector.Selected == actor)
-                        flags |= ImGuiTreeNodeFlags.Selected;
-
-                    if (!actor.IsActiveInHierarchy)
-                    {
-                        ImGui.PushStyleColor(ImGuiCol.Text, new Vector4(0.5f));
-                    }
-
-                    bool open = ImGui.TreeNodeEx("##node", flags);
-
-                    var isClicked = ImGui.IsItemClicked();
-                    ImGui.SameLine();
-                    var iconCursorX = ImGui.GetCursorPosX();
-                    var iconOffset = 8;
-                    ImGui.SetCursorPosX(iconCursorX - iconOffset);
-                    EditorImGui.ImageFromIcon(EditorIcon.Actor2, new GlmNet.vec2(18, 18));
-
-                    ImGui.SameLine();
-                    iconCursorX = ImGui.GetCursorPosX();
-                    ImGui.SetCursorPosX(iconCursorX + iconOffset - 4);
-                    ImGui.TextUnformatted(actor.Name);
-
-                    if (!actor.IsActiveInHierarchy)
-                        ImGui.PopStyleColor();
-
-                    if (isClicked)
-                        Selector.Selected = actor;
-
-                    if (EditorImGui.BeginPopupContextItem("ActorContext"))
-                    {
-                        Selector.Selected = actor;
-
-                        if (ImGui.MenuItem("Create Actor"))
-                        {
-                            new Actor("Actor").Transform.Parent = actor.Transform;
-                        }
-
-                        ImGui.Separator();
-
-                        if (ImGui.MenuItem("Rename"))
-                        {
-                        }
-
-                        if (ImGui.MenuItem("Duplicate"))
-                        {
-                        }
-                        ImGui.BeginDisabled(!actor.Transform.Parent);
-                        if (ImGui.MenuItem("Unparent to previous"))
-                        {
-                            if (actor.Transform.Parent.Parent)
-                            {
-                                actor.Transform.Parent = actor.Transform.Parent.Parent;
-                            }
-                            else
-                            {
-                                actor.Transform.Parent = null;
-                            }
-                        }
-                        ImGui.EndDisabled();
-                        ImGui.Separator();
-                        if (ImGui.MenuItem("Delete"))
-                        {
-                            ImGui.EndPopup();
-                            if (open && hasChildren)
-                            {
-                                ImGui.TreePop();
-                            }
-                            ImGui.PopID();
-
-                            Actor.Destroy(actor);
-                            return;
-                        }
-
-                        ImGui.EndPopup();
-                    }
-
-                    if (open && hasChildren)
-                    {
-                        for (int i = 0; i < actor.Transform.Children.Count; i++)
-                        {
-                            var child = actor.Transform.Children[i];
-                            DrawActor(child.Actor);
-                        }
-
-                        ImGui.TreePop();
-                    }
-
-                    ImGui.PopID();
-                }
 
                 for (int i = SceneManager.Scenes.Count - 1; i >= 0; i--)
                 {
                     ImGui.PushID(SceneManager.Scenes[i].GetID().ToString());
                     ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags.OpenOnArrow | ImGuiTreeNodeFlags.SpanFullWidth | ImGuiTreeNodeFlags.DefaultOpen;
+
 
                     bool open = ImGui.TreeNodeEx("##scene_node", flags);
                     bool isClicked = ImGui.IsItemClicked();
@@ -187,6 +87,127 @@ namespace Editor.Views
                 Actor.Destroy(actor);
             }
             OnEndWindow();
+        }
+
+        private void DrawActor(Actor actor)
+        {
+            ImGui.PushID(actor.GetID().ToString());
+
+            bool hasChildren = actor.Transform.Children.Count > 0;
+
+            ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags.OpenOnArrow | ImGuiTreeNodeFlags.SpanFullWidth;
+
+            if (!hasChildren)
+                flags |= ImGuiTreeNodeFlags.Leaf | ImGuiTreeNodeFlags.NoTreePushOnOpen;
+
+            if (Selector.Selected && Selector.Selected == actor)
+                flags |= ImGuiTreeNodeFlags.Selected;
+
+            if (!actor.IsActiveInHierarchy)
+            {
+                ImGui.PushStyleColor(ImGuiCol.Text, new Vector4(0.5f));
+            }
+
+            ImGui.PushStyleColor(ImGuiCol.HeaderActive, EditorColors.MainColor.ToVector4());
+            bool isSelected = Selector.Selected && Selector.Selected == actor;
+            var headerColor = ImGui.GetStyle().Colors[(int)ImGuiCol.Header];
+            if (!isSelected)
+            {
+                ImGui.PushStyleColor(ImGuiCol.Header, headerColor);
+                ImGui.PushStyleColor(ImGuiCol.HeaderHovered, headerColor);
+            }
+            else
+            {
+                ImGui.PushStyleColor(ImGuiCol.Header, EditorColors.MainColor.ToVector4());
+                ImGui.PushStyleColor(ImGuiCol.HeaderHovered, EditorColors.MainColor.ToVector4());
+            }
+            ImGui.PushStyleColor(ImGuiCol.Text, new Vector4(1.0f, 1.0f, 1.0f, 1.0f));
+            bool open = ImGui.TreeNodeEx("##node", flags);
+            if (EditorImGui.BeginPopupContextItem("ActorContext"))
+            {
+                Selector.Selected = actor;
+
+                if (ImGui.MenuItem("Create Actor"))
+                {
+                    new Actor("Actor").Transform.Parent = actor.Transform;
+                }
+
+                ImGui.Separator();
+
+                if (ImGui.MenuItem("Rename"))
+                {
+                }
+
+                if (ImGui.MenuItem("Duplicate"))
+                {
+                }
+                ImGui.BeginDisabled(!actor.Transform.Parent);
+                if (ImGui.MenuItem("Clear parent"))
+                {
+                    actor.Transform.Parent = null;
+                }
+                if (ImGui.MenuItem("Unparent to previous"))
+                {
+                    if (actor.Transform.Parent.Parent)
+                    {
+                        actor.Transform.Parent = actor.Transform.Parent.Parent;
+                    }
+                    else
+                    {
+                        actor.Transform.Parent = null;
+                    }
+                }
+                ImGui.EndDisabled();
+                ImGui.Separator();
+                if (ImGui.MenuItem("Delete"))
+                {
+                    ImGui.EndPopup();
+                    if (open && hasChildren)
+                    {
+                        ImGui.TreePop();
+                    }
+                    ImGui.PopID();
+
+                    Actor.Destroy(actor);
+                    return;
+                }
+
+                ImGui.EndPopup();
+            }
+
+            ImGui.PopStyleColor(4);
+
+            var isClicked = ImGui.IsItemClicked();
+            ImGui.SameLine();
+            var iconCursorX = ImGui.GetCursorPosX();
+            var iconOffset = 8;
+            ImGui.SetCursorPosX(iconCursorX - iconOffset);
+            EditorImGui.ImageFromIcon(EditorIcon.Actor2, new GlmNet.vec2(18, 18));
+
+            ImGui.SameLine();
+            iconCursorX = ImGui.GetCursorPosX();
+            ImGui.SetCursorPosX(iconCursorX + iconOffset - 4);
+            ImGui.TextUnformatted(actor.Name);
+
+            if (!actor.IsActiveInHierarchy)
+                ImGui.PopStyleColor();
+
+            if (isClicked)
+                Selector.Selected = actor;
+
+           
+            if (open && hasChildren)
+            {
+                for (int i = 0; i < actor.Transform.Children.Count; i++)
+                {
+                    var child = actor.Transform.Children[i];
+                    DrawActor(child.Actor);
+                }
+
+                ImGui.TreePop();
+            }
+
+            ImGui.PopID();
         }
     }
 }
